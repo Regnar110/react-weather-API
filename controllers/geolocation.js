13170d2a64e3b5382e6a3930361c9507e11cda3d
Mateusz <66961646+Regnar110@users.noreply.github.com>
2022-12-15 17:43:26 +0100

    fixed controller function. Added to this controller ability to fetch current weather for the initial user location

--- controllers/geolocation.js
+++ controllers/geolocation.js
@@ -1,11 +1,27 @@
-const geoLocation = async (res, req) => {
-    console.log(req)
+const NodeGeocoder = require('node-geocoder');
+require('dotenv').config();
+const geoCoderoptions = {
+    provider: 'google',
+    apiKey: process.env.REVERSE_GEO_LOCATION_KEY
+}
+
+const geocoder = NodeGeocoder(geoCoderoptions);
+
+const geoLocation = async (req, res) => {
     const {lat, lon} = req.body;
     const resPosition = await geocoder.reverse({ lat: lat, lon: lon });
     const {city, country} = resPosition[0];
+    const getCityLocationKey = await fetch(`http://dataservice.accuweather.com/locations/v1/search.json?q=${city}&apikey=${process.env.ACCU_WEATHER_KEY}`);
+    const cityLocationKey = await getCityLocationKey.json();
+    const {Key} = cityLocationKey[0];
+    const getCurrentWeatherForPosition = await fetch(`http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${process.env.ACCU_WEATHER_KEY}&details=true`);
+    const currentWeather = await getCurrentWeatherForPosition.json();
+
+    console.log(Key, currentWeather[0])
     res.send({
         city: city,
-        country: country
+        country: country,
+        currentWeather: currentWeather[0]
     })
 }
 
--- server.js
+++ server.js
@@ -1,23 +1,13 @@
-const { geoLocation } = require('./controllers/geolocation.js')
-
+const {geoLocation} = require('./controllers/geolocation.js')
 const express = require('express');
-const NodeGeocoder = require('node-geocoder');
 const cors = require('cors');
-require('dotenv').config();
 const port = 3600;
 
-const geoCoderoptions = {
-    provider: 'google',
-    apiKey: process.env.REVERSE_GEO_LOCATION_KEY,
-  }
-
 const app = express();
-const geocoder = NodeGeocoder(geoCoderoptions);
 app.use(express.json()) // pozwala odczytać obiekt "body" requestu
 app.use(cors());
 
-
-app.post('/geolocation', (res, req) => geoLocation(res, req))
+app.post('/initCurrentUserGeoPosition', geoLocation)
 
 app.listen(port, () =>{
     console.log(`server listening on ${port}`)
