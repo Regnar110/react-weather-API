34957243fce5cd080b3e34c263d976694ac918e1
Mateusz <66961646+Regnar110@users.noreply.github.com>
2022-12-15 21:14:34 +0100

    added new weather API - last not working

--- controllers/geolocation.js
+++ controllers/geolocation.js
@@ -6,23 +6,40 @@ const geoCoderoptions = {
 }
 
 const geocoder = NodeGeocoder(geoCoderoptions);
+const current_date = new Date()
+const end_date = new Date(new Date().setDate(new Date().getDate() + 7))
+
+const returnOneDayWeatherConditions = (data) => {
+    delete data.daily.time
+    delete data.hourly.time
+    const { sunrise, sunset } = data.daily;
+    const { apparent_temperature, cloudcover, rain, snowfall, visibility, weathercode} = data.hourly;
+    const newWeatherConditionsArray = [apparent_temperature, cloudcover, rain, snowfall, visibility, weathercode].map(el => {
+        const newElement = el.splice(24, 145)
+        return newElement
+        })
+    const newSunConditions = [sunrise, sunset].map(el => {
+        const newSunConditions = el.splice(1, 6)
+        return newSunConditions
+    })
+    return [newWeatherConditionsArray, newSunConditions];
+}
 
 const geoLocation = async (req, res) => {
     const {lat, lon} = req.body;
     const resPosition = await geocoder.reverse({ lat: lat, lon: lon });
     const {city, country} = resPosition[0];
-    const getCityLocationKey = await fetch(`http://dataservice.accuweather.com/locations/v1/search.json?q=${city}&apikey=${process.env.ACCU_WEATHER_KEY}`);
-    const cityLocationKey = await getCityLocationKey.json();
-    const {Key} = cityLocationKey[0];
-    const getCurrentWeatherForPosition = await fetch(`http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${process.env.ACCU_WEATHER_KEY}&details=true`);
-    const currentWeather = await getCurrentWeatherForPosition.json();
-
-    console.log(Key, currentWeather[0])
+    const getWeatherConditionsForCurrentCity = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=13.41&current_weather=true&hourly=rain,weathercode,visibility,snowfall,cloudcover,apparent_temperature&daily=sunrise,sunset&timezone=GMT`);
+    const weatherConditions = await getWeatherConditionsForCurrentCity.json();
+    returnOneDayWeatherConditions(weatherConditions);
     res.send({
         city: city,
         country: country,
-        currentWeather: currentWeather[0]
+        currentWeather: weatherConditions
     })
 }
 
 module.exports = {geoLocation}
+
+// Mamy endpoint dla opcji w której użytkownikowi łądowana jest pogoda dla pozycji jego urządzenia
+// teraz należy stworzyć endpoint dla użytkownika, który będzie chciał po nazwie miejscowości wyszukać jakieś miejsce i uzyskać dla niego dane pogodowe.
