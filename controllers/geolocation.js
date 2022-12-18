ea36ca5ff983bd1c4a543a0e36464a622bf8ea76
Mateusz <66961646+Regnar110@users.noreply.github.com>
2022-12-18 22:11:52 +0100

    added new function for controller which allow user fetch data for his city

--- controllers/geolocation.js
+++ controllers/geolocation.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const NodeGeocoder = require('node-geocoder');
 require('dotenv').config();
 const geoCoderoptions = {
@@ -6,40 +7,77 @@ const geoCoderoptions = {
 }
 
 const geocoder = NodeGeocoder(geoCoderoptions);
-const current_date = new Date()
-const end_date = new Date(new Date().setDate(new Date().getDate() + 7))
-
-const returnOneDayWeatherConditions = (data) => {
-    delete data.daily.time
-    delete data.hourly.time
-    const { sunrise, sunset } = data.daily;
-    const { apparent_temperature, cloudcover, rain, snowfall, visibility, weathercode} = data.hourly;
-    const newWeatherConditionsArray = [apparent_temperature, cloudcover, rain, snowfall, visibility, weathercode].map(el => {
-        const newElement = el.splice(24, 145)
-        return newElement
-        })
-    const newSunConditions = [sunrise, sunset].map(el => {
-        const newSunConditions = el.splice(1, 6)
-        return newSunConditions
-    })
-    return [newWeatherConditionsArray, newSunConditions];
-}
+const current_date = new Date().toISOString().split('T')[0]
+const end_date = new Date(new Date().setDate(new Date().getDate() + 7)).toISOString().split('T')[0]
 
 const geoLocation = async (req, res) => {
-    const {lat, lon} = req.body;
-    const resPosition = await geocoder.reverse({ lat: lat, lon: lon });
-    const {city, country} = resPosition[0];
-    const getWeatherConditionsForCurrentCity = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=13.41&current_weather=true&hourly=rain,weathercode,visibility,snowfall,cloudcover,apparent_temperature&daily=sunrise,sunset&timezone=GMT`);
-    const weatherConditions = await getWeatherConditionsForCurrentCity.json();
-    returnOneDayWeatherConditions(weatherConditions);
-    res.send({
-        city: city,
-        country: country,
-        currentWeather: weatherConditions
-    })
+    if(req.body.locationName) {
+        const latLonPosition = await geocoder.geocode({address: req.body.locationName})
+        const { city, country, latitude, longitude} = latLonPosition[0];
+        const getWeatherConditionsForCurrentCity = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&hourly=temperature_2m,apparent_temperature,rain,snowfall,weathercode,surface_pressure,cloudcover,visibility,windgusts_10m&daily=weathercode,sunrise,sunset&timezone=GMT&start_date=${current_date}&end_date=${end_date}`);
+        const weatherConditions = await getWeatherConditionsForCurrentCity.json();
+        res.send({
+            city: city,
+            country: country,
+            currentWeather: weatherConditions
+        })
+    } else if(req.body.latitude && req.body.longitude) {
+        const {latitude, longitude} = req.body;
+        const resPosition = await geocoder.reverse({ lat: latitude, lon: longitude });
+        const {city, country} = resPosition[0];
+        const getWeatherConditionsForCurrentCity = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&hourly=temperature_2m,apparent_temperature,rain,snowfall,weathercode,surface_pressure,cloudcover,visibility,windgusts_10m&daily=weathercode,sunrise,sunset&timezone=GMT&start_date=${current_date}&end_date=${end_date}`);
+        const weatherConditions = await getWeatherConditionsForCurrentCity.json();
+        res.send({
+            city: city,
+            country: country,
+            currentWeather: weatherConditions
+        })
+    }
+
 }
 
 module.exports = {geoLocation}
 
 // Mamy endpoint dla opcji w której użytkownikowi łądowana jest pogoda dla pozycji jego urządzenia
 // teraz należy stworzyć endpoint dla użytkownika, który będzie chciał po nazwie miejscowości wyszukać jakieś miejsce i uzyskać dla niego dane pogodowe.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const returnOneDayWeatherConditions = (data) => {
+//     // // delete data.daily.time
+//     // // delete data.hourly.time
+//     // const { sunrise, sunset } = data.daily;
+//     const { apparent_temperature, cloudcover, rain, snowfall, visibility, weathercode} = data.hourly;
+//     const newWeatherConditionsArray = [apparent_temperature, cloudcover, rain, snowfall, visibility, weathercode].map(el => {
+//         const newElement = el.splice(24, 145)
+//         return newElement
+//         })
+//     const newSunConditions = [sunrise, sunset].map(el => {
+//         const newSunConditions = el.splice(1, 6)
+//         return newSunConditions
+//     })
+//     return [newWeatherConditionsArray, newSunConditions];
+// }
