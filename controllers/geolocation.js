cf5c66391bc7377c01600d286fdb5374bfc5432d
Mateusz <66961646+Regnar110@users.noreply.github.com>
2022-12-19 11:01:38 +0100

    some code cleaning in geocoding controller functions

--- controllers/geolocation.js
+++ controllers/geolocation.js
@@ -10,28 +10,41 @@ const geocoder = NodeGeocoder(geoCoderoptions);
 const current_date = new Date().toISOString().split('T')[0]
 const end_date = new Date(new Date().setDate(new Date().getDate() + 7)).toISOString().split('T')[0]
 
+const geoCodingFunction = async (geoData, res) => {
+    const { city, country, latitude, longitude} = geoData;
+    const getWeatherConditionsForCurrentCity = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&hourly=temperature_2m,apparent_temperature,rain,snowfall,weathercode,surface_pressure,cloudcover,visibility,windgusts_10m&daily=weathercode,sunrise,sunset&timezone=GMT&start_date=${current_date}&end_date=${end_date}`);
+    const weatherConditions = await getWeatherConditionsForCurrentCity.json();
+    res.send({
+        city: city,
+        country: country,
+        currentWeather: weatherConditions
+    })
+}
+
 const geoLocation = async (req, res) => {
     if(req.body.locationName) {
         const latLonPosition = await geocoder.geocode({address: req.body.locationName})
         const { city, country, latitude, longitude} = latLonPosition[0];
-        const getWeatherConditionsForCurrentCity = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&hourly=temperature_2m,apparent_temperature,rain,snowfall,weathercode,surface_pressure,cloudcover,visibility,windgusts_10m&daily=weathercode,sunrise,sunset&timezone=GMT&start_date=${current_date}&end_date=${end_date}`);
-        const weatherConditions = await getWeatherConditionsForCurrentCity.json();
-        res.send({
-            city: city,
-            country: country,
-            currentWeather: weatherConditions
-        })
+        geoCodingFunction({city, country, latitude, longitude}, res)
+        // const getWeatherConditionsForCurrentCity = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&hourly=temperature_2m,apparent_temperature,rain,snowfall,weathercode,surface_pressure,cloudcover,visibility,windgusts_10m&daily=weathercode,sunrise,sunset&timezone=GMT&start_date=${current_date}&end_date=${end_date}`);
+        // const weatherConditions = await getWeatherConditionsForCurrentCity.json();
+        // res.send({
+        //     city: city,
+        //     country: country,
+        //     currentWeather: weatherConditions
+        // })
     } else if(req.body.latitude && req.body.longitude) {
         const {latitude, longitude} = req.body;
         const resPosition = await geocoder.reverse({ lat: latitude, lon: longitude });
         const {city, country} = resPosition[0];
-        const getWeatherConditionsForCurrentCity = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&hourly=temperature_2m,apparent_temperature,rain,snowfall,weathercode,surface_pressure,cloudcover,visibility,windgusts_10m&daily=weathercode,sunrise,sunset&timezone=GMT&start_date=${current_date}&end_date=${end_date}`);
-        const weatherConditions = await getWeatherConditionsForCurrentCity.json();
-        res.send({
-            city: city,
-            country: country,
-            currentWeather: weatherConditions
-        })
+        geoCodingFunction({city, country, latitude, longitude}, res)
+        // const getWeatherConditionsForCurrentCity = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&hourly=temperature_2m,apparent_temperature,rain,snowfall,weathercode,surface_pressure,cloudcover,visibility,windgusts_10m&daily=weathercode,sunrise,sunset&timezone=GMT&start_date=${current_date}&end_date=${end_date}`);
+        // const weatherConditions = await getWeatherConditionsForCurrentCity.json();
+        // res.send({
+        //     city: city,
+        //     country: country,
+        //     currentWeather: weatherConditions
+        // })
     }
 
 }
