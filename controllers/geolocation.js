12abc629270a4638f05b4891a8a47ede6bbf0486
Mateusz <66961646+Regnar110@users.noreply.github.com>
2023-01-02 01:10:17 +0100

    added query parameters to fetch more data from open-meteo API

--- controllers/geolocation.js
+++ controllers/geolocation.js
@@ -13,7 +13,7 @@ const geoCodingFunction = async (geoData, res) => {
     try{
         const { city, country, latitude, longitude} = geoData;
         console.log(`GEOCODING city: ${city}`)
-        const getWeatherConditionsForCurrentCity = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&hourly=weathercode,surface_pressure,cloudcover&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,rain_sum,showers_sum,snowfall_sum&timezone=GMT&start_date=${current_date}&end_date=${end_date}`);
+        const getWeatherConditionsForCurrentCity = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&hourly=windspeed_10m,apparent_temperature,weathercode,surface_pressure,cloudcover,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,rain_sum,showers_sum,snowfall_sum&timezone=GMT&start_date=${current_date}&end_date=${end_date}`);
         const weatherConditions = await getWeatherConditionsForCurrentCity.json();
         res.send({
             city: city,
