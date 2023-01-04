04cb9688604461d01ebccee075551bf06f8bd1b0
Mateusz <66961646+Regnar110@users.noreply.github.com>
2023-01-04 01:41:03 +0100

    added to geoCodingFunction fetch line to download time zone data to prepare place time data for current searched location for user

--- controllers/geolocation.js
+++ controllers/geolocation.js
@@ -13,12 +13,15 @@ const geoCodingFunction = async (geoData, res) => {
     try{
         const { city, country, latitude, longitude} = geoData;
         console.log(`GEOCODING city: ${city}`)
+        const getTimeForLocation = await fetch(`https://maps.googleapis.com/maps/api/timezone/json?location=${latitude},${longitude}&timestamp=1331161200&key=${process.env.REVERSE_GEO_LOCATION_KEY}`)
         const getWeatherConditionsForCurrentCity = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&hourly=windspeed_10m,apparent_temperature,weathercode,surface_pressure,cloudcover,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,rain_sum,showers_sum,snowfall_sum&timezone=GMT&start_date=${current_date}&end_date=${end_date}`);
         const weatherConditions = await getWeatherConditionsForCurrentCity.json();
+        const timeForLocation = await getTimeForLocation.json();
         res.send({
             city: city,
             country: country,
-            currentWeather: weatherConditions
+            currentWeather: weatherConditions,
+            currentLocationTime: new Date().toLocaleString("en-US", { timeZone: `${timeForLocation.timeZoneId}`, hour12: false, hour: '2-digit', minute:'2-digit'})
         })
     } catch(err) {
         res.send({
@@ -30,7 +33,7 @@ const geoCodingFunction = async (geoData, res) => {
 
 const geoLocation = async (req, res) => {
     try {
-        if(req.body.locationName) {
+        if(req.body.locationName) { // here is going to be a timeZone API initiation to get timezone and time for current searched location
             const latLonPosition = await geocoder.geocode({address: req.body.locationName})
             const { city, country, latitude, longitude} = latLonPosition[0];
             geoCodingFunction({city, country, latitude, longitude}, res)
